d2f6c691d88534076065c90b23e5a3c1dfab3dfc
Ifeanyi222 <linfitechitsolcom@gmail.com>
2024-02-24 03:32:01 +0100

    done

---
+++ user_input.py
@@ -0,0 +1,9 @@
+# program that ask the user
+# Name
+# Age
+# Location
+name=input('What is your Name? ')
+age=input('What is your Age? ')
+location=input('Where is your Location? ')
+
+print(f"Hello {name} you are {age} years old and live in {location}")
